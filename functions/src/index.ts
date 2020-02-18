c62b1e825c16652614e5a79c2acb51c413a7d936
hcarcamovasquez <hcarcamovasquez@gmail.com>
2020-02-17 22:07:44 -0300

    validaciones

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,20 +9,23 @@ sgMail.setApiKey(process.env.API_KEY_SEND_GRID || " ");
 
 const log = factory.getLogger("functions.sendmail");
 
-
 export const sendmail = functions.https.onRequest((request, response) => {
 
-    const {from, to, subject, text} = request.body;
+    let {from, to, subject, text} = request.body;
+    from = emailIsValid(from) ? from : " ";
+    to = emailIsValid(to) ? to : " ";
+    subject = validateLength(subject, 30);
+    text = validateLength(text, 150);
 
     sgMail.send({
-        from: emailIsValid(from) ? from : " ",
-        to: emailIsValid(to) ? to : " ",
+        from: from,
+        to: to,
         subject: subject,
         text: text,
         html: "<p>This is a test email</p>"
     }).then(() => {
         response.sendStatus(200);
-        log.info(() => `Sent email to: ${from} - from: ${from}`);
+        log.info(() => `Sent email from: ${from} - to: ${to}`);
 
     }, err => {
         log.error(() => err);
@@ -33,4 +36,8 @@ export const sendmail = functions.https.onRequest((request, response) => {
 
 function emailIsValid(email: string) {
     return /\S+@\S+\.\S+/.test(email)
+}
+
+function validateLength(val: string, length: number) {
+    return val.length > length ? val.substring(0, length) : val;
 }
