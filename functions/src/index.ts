21988ab988319360c734aa2f296a4b342a354cff
hcarcamovasquez <hcarcamovasquez@gmail.com>
2020-02-19 01:36:53 -0300

    cors

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,9 @@
 import * as functions from 'firebase-functions';
-import sgMail = require("@sendgrid/mail");
 import * as dotenv from 'dotenv';
 import {factory} from "./ConfigLog4j";
+import sgMail = require("@sendgrid/mail");
+
+const cors = require("cors")({origin: true});
 
 dotenv.config();
 
@@ -11,27 +13,29 @@ const log = factory.getLogger("functions.sendmail");
 
 export const sendmail = functions.https.onRequest((request, response) => {
 
-    let {from, to, subject, text} = request.body;
-    from = emailIsValid(from) ? from : " ";
-    to = emailIsValid(to) ? to : " ";
-    subject = validateLength(subject, 30);
-    text = validateLength(text, 150);
+    cors(request, response, () => {
 
-    sgMail.send({
-        from: from,
-        to: to,
-        subject: subject,
-        text: text,
-        html: "<p>This is a test email</p>"
-    }).then(() => {
-        response.sendStatus(200);
-        log.info(() => `Sent email from: ${from} - to: ${to}`);
+        let {from, to, subject, text} = request.body;
+        from = emailIsValid(from) ? from : " ";
+        to = emailIsValid(to) ? to : " ";
+        subject = validateLength(subject, 30);
+        text = validateLength(text, 150);
 
-    }, err => {
-        log.error(() => err);
-        response.sendStatus(404);
-    });
+        sgMail.send({
+            from: from,
+            to: to,
+            subject: subject,
+            text: text,
+            html: "<p>This is a test email</p>"
+        }).then(() => {
+            response.sendStatus(200);
+            log.info(() => `Sent email from: ${from} - to: ${to}`);
 
+        }, err => {
+            log.error(() => err);
+            response.sendStatus(404);
+        });
+    })
 });
 
 function emailIsValid(email: string) {
